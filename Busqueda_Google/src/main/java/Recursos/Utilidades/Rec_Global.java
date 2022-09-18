0924f8817dff11d488c5d32d84c21852e507bf71
Jose Vilchez <jhtgus971454@gmail.com>
2022-09-18 16:10:44 -0500

    1809

---
+++ Busqueda_Google/src/main/java/Recursos/Utilidades/Rec_Global.java
@@ -0,0 +1,20 @@
+package Recursos.Utilidades;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Rec_Global {
+
+    WebDriver driver;
+
+    public Rec_Global(WebDriver _driver){
+        driver= _driver;
+    }
+
+    public void Click(String xpath){
+    driver.findElement(By.xpath(xpath)).click();
+    }
+    public void Escribir(String texto, String xpath){
+        driver.findElement(By.xpath(xpath)).sendKeys(texto);
+    }
+}
---
+++ Busqueda_Google/src/test/java/Ejecuciones/BusquedaGoogle.java
@@ -0,0 +1,43 @@
+package Ejecuciones;
+
+
+import Escenarios.Mvn.Esc_ConsultarLibrerias;
+import Recursos.Navegador.Pag_Navegador;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import java.util.concurrent.TimeUnit;
+
+public class BusquedaGoogle {
+    WebDriver driver;
+
+
+    @Before
+    public void PreEjecucion() {
+        Pag_Navegador pag_Navegador = new Pag_Navegador(driver);
+        driver = pag_Navegador.AbrirNavegador();
+
+    }
+
+    @After
+    public void PostEjecucion() {
+        driver.close();
+
+    }
+
+    @Test
+    public void RealizarConsultaGoogle() {
+        Esc_ConsultarLibrerias esc_consultarLibrerias = new Esc_ConsultarLibrerias(driver);
+        esc_consultarLibrerias.ConsultarLibreriaMvn(driver);
+        try {
+            Thread.sleep(2000);
+        } catch (Exception fallo) {
+
+
+        }
+    }
+}
+
