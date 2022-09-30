5ed4614fcd645fff73b406fb4feba0ac90e984f7
Jose Vilchez <jhtgus971454@gmail.com>
2022-09-30 16:31:34 -0500

    1631jueves

---
+++ PruebaFacebookMejorandoCodigo/src/main/java/Escenarios/Facebook/Esc_PaginaInicioFacebook.java
@@ -0,0 +1,25 @@
+package Escenarios.Facebook;
+
+import Recursos.Utilidades.Rec_Global;
+import org.openqa.selenium.WebDriver;
+
+public class Esc_PaginaInicioFacebook {
+
+    WebDriver driver;
+
+    public Esc_PaginaInicioFacebook(WebDriver _driver){
+
+        driver = _driver;
+    }
+    public void ConsultarIngresoFacebook(WebDriver driver, String clave, String usuario){
+
+        Rec_Global rec_global = new Rec_Global(driver);
+        String ciudadOrigen = "//*[@name='email']";
+        String ciudadDestino = "//*[@name='pass']";
+
+
+        rec_global.Escribir(usuario, ciudadOrigen);
+        rec_global.Escribir(clave, ciudadDestino);
+
+    }
+}
